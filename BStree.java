1155f049e426fdfbc2e5bcfc9d43e7190c8ce0f4
Alberto Cen Zheng <AlbertoCenZheng@users.noreply.github.com>
2017-04-30 14:16:54 -0400

    Add files via upload
    Initial commit

---
+++ BStree.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author Alberto Cen Zheng
+ * Date Apr/30th/2017
+ * Class CSI213
+ * Prof Dr.Magnus
+ * 
+ * Create a tree to keep track of the frequency of the work appear 
+ * in the file
+ *
+ */
+public class BStree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
